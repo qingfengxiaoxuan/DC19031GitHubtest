390de7adbbcf1819b6345f03176b116126071a58
qingfengxiaoxuan <836261253@qq.com>
2019-07-16 19:57:59 +0800

    调试代码

---
+++ githubtest.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("this code form /home/linux/GitHub/\n")
+	return 0;
+}
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("一个补充测试代码")
+	return 0;
+}
